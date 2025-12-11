f6468b1df3f5a004e1eedc5ef79c5937f9549c2c
yuriy tretiakov <ytretiakov@gmail.com>
2025-12-11 16:14:26 +0300

    I love you

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 3040;
 const server = http.createServer((req, res) => { 
 res.statusCode = 200; 
 res.setHeader('Content-Type', 'text/plain'); 
-res.end('Hello World'); 
+res.end('Зай, я тебя люблю!'); 
 }); 
 server.listen(port, hostname, () => { 
 console.log(`Server running at http://${hostname}:${port}/`); 
